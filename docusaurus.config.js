b0f1c2a72badab7ebcc482e3f4061c44cfcf51a7
ivetha Jayaveeran <nivetha.jayaveeran@mnscorp.com>
2023-02-27 12:19:40 +0530

    project name changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NivethaJayaveeran', // Usually your GitHub org/user name.
-  projectName: 'nivetha.github.io', // Usually your repo name.
+  projectName: 'NivethaJayaveeran.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
